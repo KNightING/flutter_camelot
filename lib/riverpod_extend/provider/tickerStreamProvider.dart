c5ba8752af72a6d746d4a03fa12aecca5043fce8
keter_ciou <keter_ciou@asus.com>
2023-03-08 14:17:45 +0800

    feat: tickerStreamProvider 秒效正

--- lib/riverpod_extend/provider/tickerStreamProvider.dart
+++ lib/riverpod_extend/provider/tickerStreamProvider.dart
@@ -2,11 +2,20 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 StreamProvider<DateTime> tickerStreamProvider({required Duration duration}) {
   return StreamProvider<DateTime>((ref) async* {
-    final ticker = Stream.periodic(duration, (x) => DateTime.now());
-    ref.onDispose(() {});
-    yield DateTime.now();
-    await for (final dateTime in ticker) {
-      yield dateTime;
+    var now = DateTime.now();
+    yield now;
+    if (duration.inSeconds <= 60 && now.second != 0) {
+      // 校正
+      await Future.delayed(Duration(seconds: 60 - now.second));
+      yield DateTime.now();
     }
+
+    final ticker = Stream.periodic(duration, (x) => DateTime.now());
+    final listen = ticker.listen((event) {});
+    ref.onDispose(() {
+      listen.cancel();
+    });
+
+    yield* ticker;
   });
 }
