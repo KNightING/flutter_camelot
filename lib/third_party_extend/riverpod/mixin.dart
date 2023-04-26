c9c95d0ddb15de11c83ae5ca6c0d1a2d0467b4f5
keter_ciou <keter_ciou@asus.com>
2023-04-26 12:59:52 +0800

    feat: riverpod mixin _refresh rename to init and override build

--- lib/third_party_extend/riverpod/mixin.dart
+++ lib/third_party_extend/riverpod/mixin.dart
@@ -3,35 +3,50 @@ import 'dart:async';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 mixin AutoDisposeAsyncNotifierRefresh<T> on AutoDisposeAsyncNotifier<T> {
-  FutureOr<T> _refresh();
+  FutureOr<T> init();
+
+  @override
+  FutureOr<T> build() {
+    return init();
+  }
 
   void refresh() async {
     state = const AsyncValue.loading();
     state = await AsyncValue.guard(() async {
-      return await _refresh();
+      return await init();
     });
   }
 }
 
 mixin AutoDisposeFamilyAsyncNotifierRefresh<T, A>
-on AutoDisposeFamilyAsyncNotifier<T, A> {
-  FutureOr<T> _refresh();
+    on AutoDisposeFamilyAsyncNotifier<T, A> {
+  FutureOr<T> init(A arg);
+
+  @override
+  FutureOr<T> build(A arg) {
+    return init(arg);
+  }
 
   void refresh() async {
     state = const AsyncValue.loading();
     state = await AsyncValue.guard(() async {
-      return await _refresh();
+      return await init(arg);
     });
   }
 }
 
 mixin AsyncNotifierRefresh<T> on AsyncNotifier<T> {
-  FutureOr<T> _refresh();
+  FutureOr<T> init();
+
+  @override
+  FutureOr<T> build() {
+    return init();
+  }
 
   void refresh() async {
     state = const AsyncValue.loading();
     state = await AsyncValue.guard(() async {
-      return await _refresh();
+      return await init();
     });
   }
 }
