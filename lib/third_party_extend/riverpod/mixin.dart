2ffb517b48538e55d40a820fc095ef455546c545
keter_ciou <keter_ciou@asus.com>
2023-04-26 13:54:55 +0800

    fix: fix riverpod_refresh set state to loading should not use const

--- lib/third_party_extend/riverpod/mixin.dart
+++ lib/third_party_extend/riverpod/mixin.dart
@@ -11,8 +11,10 @@ mixin AutoDisposeAsyncNotifierRefresh<T> on AutoDisposeAsyncNotifier<T> {
   }
 
   void refresh() async {
-    state = const AsyncValue.loading();
-    state = await AsyncValue.guard(() async {
+    // can't use `const AsyncValue.loading()` in here because it's type is AsyncValue<Never>.
+    // should use `AsyncValue<T>.loading()` or `AsyncLoading<T>()`
+    state = AsyncValue<T>.loading();
+    state = await AsyncValue.guard<T>(() async {
       return await init();
     });
   }
@@ -28,8 +30,8 @@ mixin AutoDisposeFamilyAsyncNotifierRefresh<T, A>
   }
 
   void refresh() async {
-    state = const AsyncValue.loading();
-    state = await AsyncValue.guard(() async {
+    state = AsyncValue<T>.loading();
+    state = await AsyncValue.guard<T>(() async {
       return await init(arg);
     });
   }
@@ -44,9 +46,25 @@ mixin AsyncNotifierRefresh<T> on AsyncNotifier<T> {
   }
 
   void refresh() async {
-    state = const AsyncValue.loading();
-    state = await AsyncValue.guard(() async {
+    state = AsyncValue<T>.loading();
+    state = await AsyncValue.guard<T>(() async {
       return await init();
+    });
+  }
+}
+
+mixin FamilyAsyncNotifierRefresh<T, A> on FamilyAsyncNotifier<T, A> {
+  FutureOr<T> init(A arg);
+
+  @override
+  FutureOr<T> build(A arg) {
+    return init(arg);
+  }
+
+  void refresh() async {
+    state = AsyncValue<T>.loading();
+    state = await AsyncValue.guard<T>(() async {
+      return await init(arg);
     });
   }
 }
